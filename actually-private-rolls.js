c95ce2522058689928bef20ca5a4717db7ff8ba6
Felix <syl3r31@gmail.com>
2019-11-02 19:47:40 +0100

    Add files via upload

--- actually-private-rolls.js
+++ actually-private-rolls.js
@@ -1,5 +1,5 @@
 Hooks.on('init', () => {
-    game.settings.register("chatfix", "hidePrivateRolls", {
+    game.settings.register("actually-private-rolls", "hidePrivateRolls", {
         name: "Hide Private Rolls",
         hint: "Enable this to hide Private GM Rolls unless they are GM or the one that rolled.",
         scope: "world",
@@ -11,7 +11,7 @@ Hooks.on('init', () => {
 
 Hooks.on('renderChatMessage', (app, msg, html) => {
 
-    if (game.settings.get('chatfix', 'hidePrivateRolls') && msg.isWhisper === 1) {
+    if (game.settings.get('actually-private-rolls', 'hidePrivateRolls') && msg.isWhisper === 1) {
         if (game.user.isGM === false && game.user.data._id !== msg.author.data._id) {
             console.log('hide message');
             html.hide();
