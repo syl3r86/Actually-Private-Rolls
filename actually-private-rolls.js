242813d15e12c1dae34c0010edf9bf91edc350ff
Felix <syl3r31@gmail.com>
2020-03-31 13:45:42 +0200

    Add files via upload

--- actually-private-rolls.js
+++ actually-private-rolls.js
@@ -10,9 +10,8 @@ Hooks.on('init', () => {
 });
 
 Hooks.on('renderChatMessage', (app, html, msg) => {
-
-    if (game.settings.get('actually-private-rolls', 'hidePrivateRolls') && msg.isWhisper === 1) {
-        if (game.user.isGM === false && game.user.data._id !== msg.author.data._id) {
+    if (game.settings.get('actually-private-rolls', 'hidePrivateRolls') && msg.whisperTo !== '') {
+        if (game.user.isGM === false && game.user.data._id !== msg.author.data._id && msg.message.whisper.indexOf(game.user.id) === -1) {
             html.hide();
         }
     }
